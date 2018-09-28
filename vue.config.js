5e80cb62eb80ff3255620d14ca24e23b81a68c64
Caaalabash <1121062986@qq.com>
2018-09-28 22:36:38 +0800

    feat: 重新添加bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   devServer: {
     proxy: process.env.VUE_APP_SERVICE
   },
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new BundleAnalyzerPlugin()
+        ]
+      }
+    }
+  }
 }
