8d9c4cb0af99c021b261e95bb35409bd09991d20
Caaalabash <1121062986@qq.com>
2018-10-06 01:02:51 +0800

    fix: 注册notify组件

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import {
   Input,
   Pagination,
   Dialog,
+  Notification,
   Upload,
   Menu,
   Submenu,
@@ -59,6 +60,7 @@ Vue.use(Loading.directive);
 
 Vue.prototype.$loading = Loading.service;
 Vue.prototype.$message = Message;
+Vue.prototype.$notify = Notification;
 
 Vue.directive('touch',touchDirective)
 Vue.directive('resize',resizeDirective)
