58f2db89ab4efb43d0f6c42a7dd3fe4c0050f953
Caaalabash <1121062986@qq.com>
2019-03-09 23:03:55 +0800

    chore: aliyun oss

---
+++ src/service-worker.js
@@ -0,0 +1,49 @@
+// 从OSS上获取Workbox资源
+importScripts("https://static.calabash.top/workbox-v4.0.0/workbox-sw.js");
+workbox.setConfig({
+  modulePathPrefix: 'https://static.calabash.top/workbox-v4.0.0'
+});
+
+// ServiceWorker跳过waiting生命周期并在激活后立即控制客户端
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+// 加载预缓存资源
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+// 清除过期资源
+workbox.precaching.cleanupOutdatedCaches();
+// 指定APP SHELL
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"));
+// 缓存图片资源
+workbox.routing.registerRoute(
+  /\.(?:png|svg|webp|jpg|jpeg|gif)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    "cacheName":"calabash-blog-media",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 864000,
+        purgeOnQuotaError: false
+      })
+    ]
+  }),
+  'GET'
+);
+// 缓存API请求
+workbox.routing.registerRoute(
+  /api/,
+  new workbox.strategies.NetworkFirst({
+    "cacheName":"calabash-blog-api",
+    "networkTimeoutSeconds":7,
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 10,
+        maxAgeSeconds: 60,
+        purgeOnQuotaError: false
+      })
+    ]
+  }),
+  'GET'
+);
--- vue.config.js
+++ vue.config.js
@@ -1,47 +1,32 @@
-const { GenerateSW } = require('workbox-webpack-plugin')
+const path = require('path')
+const { InjectManifest } = require('workbox-webpack-plugin')
+const WebpackAliyunOssPlugin = require('webpack-aliyun-oss-plugin')
+
 
 module.exports = {
   devServer: {
     proxy: process.env.VUE_APP_SERVICE
   },
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://static.calabash.top/blog' : '/',
   productionSourceMap: false,
   configureWebpack: config => {
     if(process.env.NODE_ENV === 'production') {
       return {
         plugins: [
-          new GenerateSW ({
+          new InjectManifest({
+            importWorkboxFrom: 'disabled',
             swDest: 'service-worker.js',
-            importWorkboxFrom: 'local',
-            skipWaiting: true,
-            clientsClaim: true,
-            cleanupOutdatedCaches: true,
-            navigateFallback: './index.html',
-            dontCacheBustUrlsMatching: /\.\w{8}\./,
-            runtimeCaching: [
-              {
-                urlPattern: /\.(?:png|svg|webp|jpg|jpeg|gif)$/,
-                handler: 'staleWhileRevalidate',
-                options: {
-                  cacheName: 'calabash-blog-media',
-                  expiration: {
-                    maxEntries: 60,
-                    maxAgeSeconds: 10 * 24 * 60 * 60
-                  },
-                },
-              },
-              {
-                urlPattern: /api/,
-                handler: 'networkFirst',
-                options: {
-                  networkTimeoutSeconds: 7,
-                  cacheName: 'calabash-blog-api',
-                  expiration: {
-                    maxEntries: 10,
-                    maxAgeSeconds: 60
-                  },
-                },
-              },
-            ],
+            swSrc: path.join('./src', 'service-worker.js'),
+            dontCacheBustUrlsMatching: /\.\w{8}\./
+          }),
+          new WebpackAliyunOssPlugin({
+            region: 'oss-cn-beijing',
+            bucket: 'calabash-static',
+            ak: process.env.ossak,
+            sk: process.env.osssk,
+            filter: function (asset) {
+              return !/(\.html|\.map)$/.test(asset)
+            },
           })
         ]
       }
