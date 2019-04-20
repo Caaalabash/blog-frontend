cfc7dec89d30ef184428a90ca1a74bddd7ec6128
Caaalabash <1121062986@qq.com>
2019-04-20 17:37:54 +0800

    refactor: api & chat controller

--- server/controller/api.js
+++ server/controller/api.js
@@ -1,78 +1,77 @@
 module.exports = app => {
   const { blogModel, logModel } = app.model
   const { rsp, redisTool } = app.helper
-  /**
-   * @description 早期对MongoDB操作不熟悉. 暂未重构
-   */
+  const ONE_DAY = 1000 * 60 * 60 * 24
+
   return {
     // 分析文章发布时间
-    analyzeBlogDate(req, res) {
-      blogModel.find({ 'author': 'Calabash' }, { blogDate: 1, _id: 0 },function (e, doc) {
-        if(doc) {
-          const arr = doc.reduce((acc, item) => {
-            acc.push(item.blogDate.slice(8, 10))
-            return acc
-          }, [])
-          let cache = {}
-          for(let n of arr){
-            cache[n] ? cache[n]++ : cache[n] = 1
-          }
-          const data = Object.keys(cache).reduce((acc, key) => {
-            acc.push({ value: cache[key], name: `${key}点` })
-            return acc
-          }, [])
-          return res.json(rsp(0, data, ''))
-        }
-      })
-    },
-    // 最近十天访问记录
-    async getPvLog(req, res) {
-      const oneDay = 1000 * 60 * 60 * 24
-      let date = new Date().getTime()
-      let list = [],
-        index = 10,
-        data = []
-      while (index > 0){
-        let time = new Date(date - index * oneDay).toLocaleString('zh').split(' ')[0].replace(/\//g, '-')
-        list.push(time)
-        index --
+    async analyzeBlogDate(req, res) {
+      const doc = await blogModel.find({ 'author': 'Calabash' }, { blogDate: 1, _id: 0 })
+      if (!doc) return res.json(rsp(1, '', '暂无文章'))
+
+      const blogDateArr = doc.map(({ blogDate }) => blogDate.slice(8, 10))
+      const timeMap = {}
+      for(let date of blogDateArr){
+        timeMap[date] ? timeMap[date]++ : timeMap[date] = 1
       }
-      for (let n of list) {
-        let len = await redisTool.getIpLog(n)
-        let arr = len.map(item => item.split('-')[0])
-        data.push([...new Set(arr)].length)
-      }
+      const data = Object.keys(timeMap).reduce((acc, key) => {
+        acc.push({ value: timeMap[key], name: `${key}点` })
+        return acc
+      }, [])
+
       return res.json(rsp(0, data, ''))
     },
-    //记录接口数据到数据库
+    // 最近十天访问量
+    async getPvLog(req, res) {
+      // 组装一个连续10天的日期数组 ['2019-1-1', '2019-1-2', ...]
+      const date = new Date().getTime()
+      const list = []
+      const data = []
+      // 索引靠前, 日期越大
+      for (let i = 0; i < 10; i++) {
+        const time = new Date(date - i * ONE_DAY).toLocaleString('zh').split(' ')[0].replace(/\//g, '-')
+        list.push(time)
+      }
+      // PVLog中每一条数据格式为: 访问Ip-访问时间-请求路径
+      const allPvLogs = await Promise.all(list.map(date => redisTool.getIpLog(date)))
+      for (let eachDayLogs of allPvLogs) {
+        const ipList = eachDayLogs.map(item => item.split('-')[0])
+        data.push([...new Set(ipList)].length)
+      }
+
+      return res.json(rsp(0, data.reverse(), ''))
+    },
+    // 记录接口数据到数据库
     async insertLog(obj) {
-      let res = recordApi(obj.url, obj.method)
-      if(res){
+      const shouldRecord = shouldRecordApi(obj.url, obj.method)
+      if (shouldRecord) {
         obj.url = obj.url.split('?')[0]
         await logModel.create(obj)
       }
     },
-    //获取指定接口的访问时长记录
-    async getLogByUrl(req,res) {
-      let url = req.body.url
-      if(req.body.url === '/api/v1/idea') {
-        url = /\/api\/v1\/ideas\/\d{14}/
-      }
-      let list = await logModel.find({ url }, { _id: 0, __v: 0 })
+    // 获取指定接口的访问时长记录
+    async getLogByUrl(req, res) {
+      let { url } = req.body
+      if (url === '/api/v1/idea') url = /\/api\/v1\/ideas\/\d{14}/
+      const list = await logModel.find({ url }, { _id: 0, __v: 0 })
+
       return res.json(rsp(0, list.slice(-50), ''))
     },
-    //处理女朋友的正则需求
+    // 处理女朋友的正则需求
     async sendMyLove(req,res) {
-      let {type,content} = req.body
-      let list = content.split(/\n/)
-      let data = ``
+      const { type, content } = req.body
+      const list = content.split(/\n/)
+      const REMOVE_N = '1'
+      const UPPERCASE = '2'
+      const CONCAT = '3'
+      let data = ''
       let deleteList = []
-      //去除/N
-      if(type==='1'){
-        data = list.map((item)=>{
-          let flag = /\\N\\N.*\\N/.test(item)
-          if(flag){
-            item = item.replace(/\\N\\N.{3,}?\\N/g,function(match){
+      // 去除/N
+      if (type === REMOVE_N) {
+        data = list.map(item => {
+          const shouldProcess = /\\N\\N.*\\N/.test(item)
+          if (shouldProcess) {
+            item = item.replace(/\\N\\N.{3,}?\\N/g, match => {
               deleteList.push(match)
               return ' '
             })
@@ -80,32 +79,26 @@ module.exports = app => {
           return item
         }).join('\n')
       }
-      //首字母大写,加句号
-      if(type==='2'){
-        data = list.map(item=>{
+      // 首字母大写,加句号
+      if (type === UPPERCASE) {
+        data = list.map(item => {
           item = item.trim()
-          //该行是否含有英文字母
-          let flag = /[a-zA-Z]/.test(item)
-          if(flag){
-            let res = ''
-            //是否是小写字母开头:)
-            let startWithWord = /^[a-z]/.test(item)
-            if(startWithWord){
-              res = `${item[0].toUpperCase()}${item.slice(1)}.`
-            }else{
-              res = `${item}.`
-            }
-            deleteList.push(item)
-            return res
-          }else{
-            return item
-          }
+          // 该行是否含有英文字母
+          const shouldProcess = /[a-zA-Z]/.test(item)
+          if (!shouldProcess) return item
+          //是否是小写字母开头:)
+          deleteList.push(item)
+          const isStartWithWord = /^[a-z]/.test(item)
+
+          return isStartWithWord
+            ? `${item[0].toUpperCase()}${item.slice(1)}.`
+            : `${item}.`
         }).join('\n')
       }
       // 去除空行加语句拼接
-      if (type === '3'){
-        let noEmptyRow = list.filter(item => item.trim() !==  '')
-        for (let n of noEmptyRow){
+      if (type === CONCAT) {
+        let noEmptyRow = list.filter(item => item.trim() !== '')
+        for (let n of noEmptyRow) {
           if(/^[a-z]/.test(n)){
             data += ` ${n}`
           } else {
@@ -113,31 +106,30 @@ module.exports = app => {
           }
         }
       }
-      return res.json(rsp(0,{
-        content:data,
-        deleteList:deleteList.join('\n')
-      },''))
+      return res.json(rsp(0, { content: data, deleteList: deleteList.join('\n') }, ''))
     }
   }
 }
 
-function recordApi(url,method){
-  if(/^\/api/.test(url)){
-    let el = [url.split('?')[0],method].join('-')
-    const rules = [
-      '/api/v1/login-POST',
-      '/api/v1/checkStatus-POST',
-      '/api/v1/userinfo-GET',
-      '/api/v1/ideas-GET',
-      '/api/v1/comment-POST',
-      '/api/v1/like-POST'
-    ]
-    if(rules.includes(el) ||  /\/api\/v1\/ideas\/\d{14}-GET/.test(el)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
+/**
+ * 是否需要记录该IP地址
+ * @param {string} url 请求地址
+ * @param {string} method 请求方法
+ * @return {boolean} 是否应当记录
+ */
+function shouldRecordApi(url, method) {
+  if (!/^\/api/.test(url)) return false
+
+  const row = [url.split('?')[0], method].join('-')
+  const extraRule = /\/api\/v1\/ideas\/\d{14}-GET/
+  const rules = [
+    '/api/v1/login-POST',
+    '/api/v1/checkStatus-POST',
+    '/api/v1/userinfo-GET',
+    '/api/v1/ideas-GET',
+    '/api/v1/comment-POST',
+    '/api/v1/like-POST',
+  ]
+
+  return rules.includes(row) || extraRule.test(row)
 }
--- server/controller/chat.js
+++ server/controller/chat.js
@@ -1,57 +1,58 @@
 module.exports = app => {
   const { chatModel } = app.model
   const { getUserProp, rsp } = app.helper
-  /**
-   * @description 早期对MongoDB操作不熟悉. 暂未重构
-   */
+
   return {
-    //增加聊天对象
+    // 增加聊天对象
     async addChatObj(req, res) {
-      const user = req.body.user
-      let data = await getUserProp(user, 'avatar')
-      if(data) {
-        return res.json(rsp(0, { to: user, avatar: data}, ''))
-      }
-      return res.json(rsp(1,'','没有该用户'))
+      const { user } = req.body
+      const userAvatar = await getUserProp(user, 'avatar')
+      if (!userAvatar)  return res.json(rsp(1,'','没有该用户'))
+
+      return res.json(rsp(0, { to: user, avatar: userAvatar }, ''))
     },
-    //获取聊天列表
+    // 获取聊天列表
     async getChatList(req, res) {
-      const user = req.query.user
-      let doc = await chatModel.find({}, { _id: 0, chatid: 1 })
-      if(doc){
-        let list = doc.reduce((acc, item) => {
-          if(item.chatid.includes(user)) acc.push(item.chatid)
-          return acc
-        }, [])
-        let data = []
-        let arr = [...new Set(list)]
-        for(let n of arr){
-          let [from, to] = n.split('_')
-          let obj = from === user ? to : from
-          let avatar = await getUserProp(obj, 'avatar')
-          data.push({
-            to: obj,
-            avatar: avatar
-          })
-        }
-        return res.json(rsp(0, data, ''))
-      }
+      const { user } = req.query
+      const reg = new RegExp(user)
+      // 正则匹配出所有含有当前用户名的chatid
+      const doc = await chatModel.find({ chatid: reg }, { _id: 0, chatid: 1 })
+      // 获得去重后的chatId数组
+      const chatidList = [...new Set(doc.map(item => item.chatid))]
+      // 分离chatid, 获得聊天对象的用户名数组
+      const userList = chatidList.reduce((acc, chatId) => {
+        const [from, to] = chatId.split('_')
+        const another = from === user ? to : from
+        if (!acc.includes(another) && another) acc.push(another)
+        return acc
+      }, [])
+      // 获得聊天对象的头像数组
+      const avatarList = await Promise.all(userList.map(user => getUserProp(user, 'avatar')))
+      const data = avatarList.reduce((acc, avatar, index) => {
+        acc.push({
+          to: userList[index],
+          avatar
+        })
+        return acc
+      }, [])
+
+      return res.json(rsp(0, data, ''))
     },
     // 最近50条聊天记录
-    getChatData(req, res){
-      const chatid = req.query.chatid
-      chatModel.find({ chatid }, { _id: 0, __v: 0 }, function(err, doc) {
-        return res.json(rsp(0, doc.slice(-50), ''))
-      })
+    async getChatData(req, res) {
+      const { chatid } = req.query
+      const doc = await chatModel.find({ chatid }, { _id: 0, __v: 0 })
+
+      return res.json(rsp(0, doc.slice(-50), ''))
     },
     // 文件上传
-    uploadPic(req,res) {
-      let path = `https://blog.calabash.top/${req.file.filename}`
+    uploadPic(req, res) {
+      const path = `https://blog.calabash.top/${req.file.filename}`
       return res.json(rsp(0, path, ''))
     },
     // 语音上传
-    uploadVoice(req,res){
-      let path = `https://blog.calabash.top/${req.file.filename}`
+    uploadVoice(req, res) {
+      const path = `https://blog.calabash.top/${req.file.filename}`
       return res.json(rsp(0, path, ''))
     }
   }
