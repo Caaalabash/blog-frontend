f2ca293e0e269352f291ac1d568ebbacef5f1caf
Caaalabash <1121062986@qq.com>
2019-06-21 22:55:32 +0800

    fix: 去除前端构建文件

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -8,13 +8,8 @@ export VUE_BLOG_TAG=$1
 docker rm -f $(docker ps -a |  grep "blog"  | awk '{print $1}')
 # 删除旧镜像
 docker rmi $(docker images |  grep "blog"  | awk '{print $3}')
-# 首先运行后端服务
-cd /mynode/vue-blog-ci/deploy/server
+# 运行后端服务
+cd /mynode/vue-blog-ci/deploy
 docker-compose up -d
-
-# 运行前端服务
-# cd /mynode/vue-blog-ci/deploy
-# docker-compose up -d
-
 # 重启Nginx
 nginx -s reload
