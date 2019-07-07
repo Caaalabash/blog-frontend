3fce14f0dac5ce1597dde6856e3783fd83b7d84e
Caaalabash <1121062986@qq.com>
2019-07-07 22:41:33 +0800

    chore: 删除旧容器

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -4,13 +4,15 @@ echo $1
 export VUE_BLOG_TAG=$1
 
 lastBlogImage=$(docker images | grep "vue-blog" | awk '{print $3}')
+# 删除旧容器
+docker rm -f $(docker ps -a |  grep "blog"  | awk '{print $1}')
 # 依次运行后端、前端服务
 cd /mynode/vue-blog-ci/deploy
-docker-compose -f docker-compose-be.yml up -d --remove-orphans
-docker-compose -f docker-compose-fe.yml up -d --remove-orphans
+docker-compose -f docker-compose-be.yml up -d
+docker-compose -f docker-compose-fe.yml up -d
 # 删除旧镜像
 if [[ ! "$lastBlogImage" ]]; then
   echo "Can't find last vue-blog imageID"
 else
-  docker rmi ${lastBlogImage}
+  docker rmi -f ${lastBlogImage}
 fi
