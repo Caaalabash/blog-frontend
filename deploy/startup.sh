9b865587e94eeb9c544ee470dc117be423ba306f
Caaalabash <1121062986@qq.com>
2019-07-08 23:58:43 +0800

    feat: use private registry
    fix: tag name error
    
    fix: CRLF to LF

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -3,15 +3,9 @@
 echo $1
 export VUE_BLOG_TAG=$1
 
-lastBlogImage=$(docker images | grep "vue-blog" | awk '{print $3}')
-# 删除旧容器
+# 删除旧容器, 镜像
 docker rm -f $(docker ps -a |  grep "blog"  | awk '{print $1}')
+docker rmi -f $(docker images | grep "blog" | awk '{print $3}')
 # 依次运行后端、前端服务
 cd /mynode/vue-blog-ci/deploy
 docker-compose up -d
-# 删除旧镜像
-if [[ ! "$lastBlogImage" ]]; then
-  echo "Can't find last vue-blog imageID"
-else
-  docker rmi -f ${lastBlogImage}
-fi
