07228365fd1ca5b78b034bf8951b8a4271db761f
Caaalabash <1121062986@qq.com>
2020-02-12 21:11:49 +0800

    fix: api path

--- src/service/apiManage.js
+++ src/service/apiManage.js
@@ -5,37 +5,37 @@ class apiManage extends BaseModule {
     super()
   }
   login(payload) {
-    return this.post('https://remake.calabash.top/user/login', payload)
+    return this.post('/user/login', payload)
   }
   logout() {
-    return this.get('https://remake.calabash.top/user/logout')
+    return this.get('/user/logout')
   }
   getUserInfo() {
-    return this.get('https://remake.calabash.top/user/info')
+    return this.get('/user/info')
   }
   // 发送新博文
   createNewIdea(obj) {
-    return this.post('https://remake.calabash.top/idea', obj)
+    return this.post('/idea', obj)
   }
   // 删除博客
   deleteIdea(id) {
-    return this.delete(`https://remake.calabash.top/idea/${id}`)
+    return this.delete(`/idea/${id}`)
   }
   // 修改博文
   changeIdea(obj) {
     const { id, ...data } = obj
-    return this.put(`https://remake.calabash.top/idea/${id}`, data)
+    return this.put(`/idea/${id}`, data)
   }
   // 获取某一个文章
   getIdea(id) {
-    return this.get(`https://remake.calabash.top/idea/${id}`)
+    return this.get(`/idea/${id}`)
   }
   // 获取博文列表
   getIdeaList(obj) {
-    return this.get('https://remake.calabash.top/ideas', obj)
+    return this.get('/ideas', obj)
   }
   upload(obj, config) {
-    return this.post('https://remake.calabash.top/idea/upload', obj, config)
+    return this.post('/idea/upload', obj, config)
   }
 }
 export default new apiManage()
