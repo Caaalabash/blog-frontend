382f9605b71b61badba09aa59d350a3fe0f15931
Caaalabash <1121062986@qq.com>
2020-03-09 21:36:48 +0800

    feat: 添加备案号

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': ['error', 2],
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
     'linebreak-style': ['error', 'unix'],
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <router-view />
+    <footer class="app-footer">
+      <a target="_blank" rel="noopener noreferrer" href="http://www.beian.miit.gov.cn">蜀ICP备17031321号</a>
+    </footer>
   </div>
 </template>
 
@@ -21,4 +24,17 @@ export default {
 <style lang="less">
   @import 'assets/style/iconfont.css';
   @import 'assets/style/index.less';
+  .app-footer {
+    position: fixed;
+    left: 10px;
+    bottom: 10px;
+    a {
+      font-size: 12px;
+      text-decoration: none;
+      color: #BBC7CC;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 </style>
