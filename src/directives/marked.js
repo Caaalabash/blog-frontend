e9662e2ff7e4c772caaf6cac42c6e31addaaf94f
Caaalabash <1121062986@qq.com>
2020-05-31 15:10:17 +0800

    style: 添加highlight.js

--- src/directives/marked.js
+++ src/directives/marked.js
@@ -1,4 +1,12 @@
 import marked from 'marked'
+import highlight from 'highlight.js'
+import 'highlight.js/styles/github.css'
+
+marked.setOptions({
+  highlight(code, lang) {
+    return highlight.highlight(lang || 'javascript', code).value
+  }
+})
 
 function update(el, binding) {
   el.innerHTML = marked(binding.value)
