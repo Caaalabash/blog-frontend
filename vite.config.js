b3c42b36db90a4221c9e322a4c52b4c01d0fd578
longyao.li <longyao.li@dmall.com>
2022-10-27 22:41:47 +0800

    test: aliyun oss

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import VitePluginOss from 'vite-plugin-oss'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'https://static.calabash.top/blog/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://static.calabash.top/blog2/' : '/',
   plugins: [
     vue(),
     vueJsx(),
@@ -18,6 +18,19 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    VitePluginOss({
+      from: './dist/**', // 上传那个文件或文件夹
+      dist: "/blog2",  // 需要上传到oss上的给定文件目录
+      region: 'oss-cn-beijing',
+      bucket: 'calabash-static',
+      accessKeyId: process.env.PROD_OSS_AK,
+      accessKeySecret: process.env.PROD_OSS_SK,
+      setOssPath: filePath => {
+        let index = filePath.lastIndexOf("dist")
+        let Path = filePath.substring(index + 4, filePath.length)
+        return Path.replace(/\\/g, "/")
+      },
     }),
   ],
   resolve: {
